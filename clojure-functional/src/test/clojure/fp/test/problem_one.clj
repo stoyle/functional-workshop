cb206470ca6fb9a25222dfab1910afd7d9456b15
Alf Kristian Stoyle <alf.kristian@gmail.com>
2012-03-03 13:17:29 +0100

    Moved all running code into test. Better usability.

--- clojure-functional/src/test/clojure/fp/test/problem_one.clj
+++ clojure-functional/src/test/clojure/fp/test/problem_one.clj
@@ -1,6 +1,31 @@
 (ns fp.test.problem-one
-  (:use [fp.problem-one] :reload-all)
   (:use [clojure.test]))
+
+; Running code
+
+(defn- multiples-of-3-or-5 [num]
+  (let [mod-of #(zero? (mod num %1))]
+    (or (mod-of 3) (mod-of 5))))
+
+(defn- sum [coll]
+  (apply + coll))
+
+(defn euler-one-sum-beneath [limit]
+  (sum (filter multiples-of-3-or-5 (range 1 limit))))
+
+(defn- sum-modof-coll [coll]
+  (reductions +
+    (filter multiples-of-3-or-5 coll)))
+
+(defn euler-one-sum-beneath-lazy [limit]
+  (last
+    (sum-modof-coll (range 1 limit))))
+
+(defn euler-one-sum-beneath-eager [limit]
+  (reduce +
+    (filter multiples-of-3-or-5 (range 1 limit))))
+
+; Test code
 
 (deftest euler-one-sum
   (is (= (euler-one-sum-beneath 10) 23))
