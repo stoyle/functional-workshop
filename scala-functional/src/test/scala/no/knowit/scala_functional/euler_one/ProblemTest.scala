bb99f1d19a01f05a1c61e4208005506fb294c2cc
Fredrik Vraalsen <fredrik@vraalsen.no>
2012-03-05 15:45:58 +0100

    Don't run Scala test with range 1..10 million by default ;)

--- scala-functional/src/test/scala/no/knowit/scala_functional/euler_one/ProblemTest.scala
+++ scala-functional/src/test/scala/no/knowit/scala_functional/euler_one/ProblemTest.scala
@@ -16,9 +16,18 @@ class ProblemTest {
   }
 
   @Test
+  def find_sum_of_all_multiples_of_3_and_5_below_1000 {
+    val numbers = (1 until 1000).view
+    val res = numbers filter (i => i % 3 == 0 || i % 5 == 0) map (_.longValue) reduce (_ + _)
+    assertEquals("Wrong sum", 233168L, res)
+  }
+
+  /*
+  @Test
   def find_sum_of_all_multiples_of_3_and_5_below_10_000_000 {
     val numbers = (1 until 10000000).view
     val res = numbers filter (i => i % 3 == 0 || i % 5 == 0) map (_.longValue) reduce (_ + _)
     assertEquals("Wrong sum", 23333331666668L, res)
   }
+  */
 }
