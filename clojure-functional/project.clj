9dafa4f9283edd3004ce7a262b27c7dcca69901f
Alf Kristian Stoyle <alf.kristian@gmail.com>
2012-03-05 18:15:49 +0100

    Moved the domain model to a common project. Also added it as dependency to all other projects.

--- clojure-functional/project.clj
+++ clojure-functional/project.clj
@@ -1,5 +1,6 @@
 (defproject fp "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [no.knowit/common "0.1-SNAPSHOT"]]
   :source-path "src/main/clojure"
   :test-path "src/test/clojure")
