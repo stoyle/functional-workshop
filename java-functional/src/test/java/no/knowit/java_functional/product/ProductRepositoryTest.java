f19c3f3cf92ba1e2e55b04ae045bddcd9279f376
Fredrik Vraalsen <fredrik@vraalsen.no>
2012-03-07 15:15:00 +0100

    Make product test more functional

--- java-functional/src/test/java/no/knowit/java_functional/product/ProductRepositoryTest.java
+++ java-functional/src/test/java/no/knowit/java_functional/product/ProductRepositoryTest.java
@@ -2,7 +2,6 @@ package no.knowit.java_functional.product;
 
 import static ch.lambdaj.Lambda.*;
 import static fj.data.List.*;
-import static ch.lambdaj.collection.LambdaCollections.*;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 import static org.junit.matchers.JUnitMatchers.*;
@@ -13,83 +12,88 @@ import java.util.LinkedList;
 import java.util.List;
 
 import org.joda.time.LocalDate;
-import org.junit.Before;
 import org.junit.Test;
 
 import fj.F;
 import fj.F2;
 
-public class ProductRepositoryTest implements ProductRepository {
+public class ProductRepositoryTest {
 
-	private List<Product> products;
+	private class ProductRepositoryImpl implements ProductRepository {
 
-	public Collection<Product> getAvailableProducts(LocalDate date) {
-		// return getAvailableProductsIterator(date);
-		return getAvailableProductsForLoop(date);
-		// return getAvailableProductsLambaJ(date);
-		// return getAvailableProductsFJ(date);
-	}
+		private List<Product> products = new LinkedList<Product>();
 
-	/*
-	 * Plain Java
-	 */
-
-	private Collection<Product> getAvailableProductsIterator(LocalDate date) {
-		for (Iterator<Product> i = products.iterator(); i.hasNext();) {
-			Product p = (Product) i.next();
-			if (!p.isAvailable(date)) {
-				i.remove();
+		public ProductRepositoryImpl(Product... products) {
+			for (Product product : products) {
+				this.products.add(product);
 			}
 		}
-		return products;
-	}
 
-	private Collection<Product> getAvailableProductsForLoop(LocalDate date) {
-		List<Product> result = new LinkedList<Product>();
-		for (Product product : products) {
-			if (product.isAvailable(date)) {
-				result.add(product);
+		public Collection<Product> getAvailableProducts(LocalDate date) {
+			// return getAvailableProductsIterator(date);
+			return getAvailableProductsForLoop(date);
+			// return getAvailableProductsLambaJ(date);
+			// return getAvailableProductsFJ(date);
+		}
+
+		/*
+		 * Plain Java
+		 */
+
+		private Collection<Product> getAvailableProductsIterator(LocalDate date) {
+			for (Iterator<Product> i = products.iterator(); i.hasNext();) {
+				Product p = (Product) i.next();
+				if (!p.isAvailable(date)) {
+					i.remove();
+				}
 			}
+			return products;
 		}
-		return result;
-	}
 
-	/*
-	 * LambdaJ
-	 */
-
-	private Collection<Product> getAvailableProductsLambaJ(LocalDate date) {
-		return filter(having(on(Product.class).isAvailable(date)), products);
-	}
-
-	/*
-	 * Functional Java
-	 */
-
-	private F2<Product, LocalDate, Boolean> isAvailableOnDate = new F2<Product, LocalDate, Boolean>() {
-		@Override
-		public Boolean f(Product p, LocalDate date) {
-			return p.isAvailable(date);
+		private Collection<Product> getAvailableProductsForLoop(LocalDate date) {
+			List<Product> result = new LinkedList<Product>();
+			for (Product product : products) {
+				if (product.isAvailable(date)) {
+					result.add(product);
+				}
+			}
+			return result;
 		}
-	};
 
-	private Collection<Product> getAvailableProductsFJ(LocalDate date) {
-		F<Product, Boolean> f = isAvailableOnDate.flip().f(date);
-		return iterableList(products).filter(f).toCollection();
+		/*
+		 * LambdaJ
+		 */
+
+		private Collection<Product> getAvailableProductsLambaJ(LocalDate date) {
+			return filter(having(on(Product.class).isAvailable(date)), products);
+		}
+
+		/*
+		 * Functional Java
+		 */
+
+		private F2<Product, LocalDate, Boolean> isAvailableOnDate = new F2<Product, LocalDate, Boolean>() {
+			@Override
+			public Boolean f(Product p, LocalDate date) {
+				return p.isAvailable(date);
+			}
+		};
+
+		private Collection<Product> getAvailableProductsFJ(LocalDate date) {
+			F<Product, Boolean> f = isAvailableOnDate.flip().f(date);
+			return iterableList(products).filter(f).toCollection();
+		}
 	}
 
 	/*
 	 * Test code below here
 	 */
 
-	@Before
-	public void populateRepository() {
-		products = new LinkedList<Product>();
-	}
-
 	@Test
 	public void empty_list_when_no_products() {
-		Collection<Product> available = getAvailableProducts(new LocalDate());
+		ProductRepository productRepo = new ProductRepositoryImpl();
+		Collection<Product> available = productRepo
+				.getAvailableProducts(new LocalDate());
 		assertThat(available.isEmpty(), is(equalTo(true)));
 	}
 
@@ -97,9 +101,9 @@ public class ProductRepositoryTest implements ProductRepository {
 	public void product_available_when_matching_date() {
 		LocalDate today = new LocalDate();
 		Product coolProduct = new Product("Cool product", today, today);
-		products.add(coolProduct);
+		ProductRepository productRepo = new ProductRepositoryImpl(coolProduct);
 
-		Collection<Product> available = getAvailableProducts(today);
+		Collection<Product> available = productRepo.getAvailableProducts(today);
 		assertThat(available.size(), is(equalTo(1)));
 		assertThat(available, hasItems(coolProduct));
 	}
@@ -108,9 +112,13 @@ public class ProductRepositoryTest implements ProductRepository {
 	public void empty_list_when_no_products_matching_date() {
 		LocalDate today = new LocalDate();
 		LocalDate yesterday = today.minusDays(1);
-		products.add(new Product("Discontinued product", yesterday, yesterday));
+		Product discontinuedProduct = new Product("Discontinued product",
+				yesterday, yesterday);
 
-		Collection<Product> available = getAvailableProducts(today);
+		ProductRepository productRepo = new ProductRepositoryImpl(
+				discontinuedProduct);
+
+		Collection<Product> available = productRepo.getAvailableProducts(today);
 		assertThat(available.isEmpty(), is(equalTo(true)));
 	}
 
@@ -120,9 +128,12 @@ public class ProductRepositoryTest implements ProductRepository {
 		LocalDate yesterday = today.minusDays(1);
 		Product discontinuedProduct = new Product("Discontinued product",
 				yesterday, yesterday);
-		products.add(discontinuedProduct);
 
-		Collection<Product> available = getAvailableProducts(yesterday);
+		ProductRepository productRepo = new ProductRepositoryImpl(
+				discontinuedProduct);
+
+		Collection<Product> available = productRepo
+				.getAvailableProducts(yesterday);
 		assertThat(available, hasItems(discontinuedProduct));
 	}
 
@@ -133,13 +144,16 @@ public class ProductRepositoryTest implements ProductRepository {
 		Product discontinuedProduct = new Product("Discontinued product",
 				yesterday, yesterday);
 		Product newProduct = new Product("New product", today, null);
-		products.add(discontinuedProduct);
-		products.add(newProduct);
 
-		Collection<Product> availableYesterday = getAvailableProducts(yesterday);
+		ProductRepository productRepo = new ProductRepositoryImpl(
+				discontinuedProduct, newProduct);
+
+		Collection<Product> availableYesterday = productRepo
+				.getAvailableProducts(yesterday);
 		assertThat(availableYesterday, hasItems(discontinuedProduct));
 
-		Collection<Product> availableToday = getAvailableProducts(today);
+		Collection<Product> availableToday = productRepo
+				.getAvailableProducts(today);
 		assertThat(availableToday, hasItems(newProduct));
 	}
 }
