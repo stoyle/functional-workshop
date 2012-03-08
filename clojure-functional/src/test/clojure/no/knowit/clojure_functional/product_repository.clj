043edae01dffdeaac6a9107c4fe08bdfa6d09500
Alf Kristian Stoyle <alf.kristian@gmail.com>
2012-03-08 19:46:54 +0100

    Better more functional implementation without atom.

--- clojure-functional/src/test/clojure/no/knowit/clojure_functional/product_repository.clj
+++ clojure-functional/src/test/clojure/no/knowit/clojure_functional/product_repository.clj
@@ -3,37 +3,31 @@
   (:import [no.knowit.java_functional.product Product])
   (:import [org.joda.time LocalDate]))
 
-(def product-list (atom []))
-
-(defn product-is-available [local-date]
+(defn product-is-available [local-date product-coll]
   (filter
-    #(.isAvailable %1 local-date) @product-list))
-
-
-(defn clear-state-fn [test-func]
-  (reset! product-list [])
-  (test-func))
-
-(use-fixtures :each clear-state-fn)
+    #(.isAvailable %1 local-date) product-coll))
 
 
 (deftest empty_list_when_no_products
-  (is (= (product-is-available (LocalDate.)) [])))
+  (is (= (product-is-available (LocalDate.) []))))
 
 (deftest product_available_when_matching_date
   (let [today (new LocalDate)
         product (new Product "Cool product" today today)]
-    (swap! product-list
-      #(conj % product))
-    (is (not-empty (product-is-available today)))
-    (is (.contains (product-is-available today) product))))
+    (is (not-empty (product-is-available today [product])))
+    (is (.contains (product-is-available today [product]) product))))
 
 (deftest empty_list_when_no_products_matching_date
   (let [today (new LocalDate)
         yesterday (.minusDays today 1)
         product (new Product "Cool product" yesterday yesterday)]
-    (swap! product-list
-      #(conj % product))
-    (is (empty (product-is-available today)))))
+    (is (empty (product-is-available today [product])))))
+
+(deftest find_discontinued_products
+  (let [yesterday (.minusDays (new LocalDate) 1)
+        product (new Product "Cool product" yesterday yesterday)]
+    (is (not-empty (product-is-available yesterday [product])))))
+
+
 
 (run-tests)
