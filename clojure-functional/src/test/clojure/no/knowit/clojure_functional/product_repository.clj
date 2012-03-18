56e46b55ee2cf783b5f4a1ae587b68b41502b083
Alf Kristian Stoyle <alf.kristian@gmail.com>
2012-03-18 17:35:31 +0100

    Making product_repository ready for solving.

--- clojure-functional/src/test/clojure/no/knowit/clojure_functional/product_repository.clj
+++ clojure-functional/src/test/clojure/no/knowit/clojure_functional/product_repository.clj
@@ -7,18 +7,12 @@
 
 ;; Use filter and provide your own function. Remember how you call a native Java method?
 (defn product-is-available [local-date product-coll]
-  (filter
-    #(.isAvailable %1 local-date) product-coll))
+  product-coll)
 
 ; Write a recursive version not using filter. This is a bit advanced. Possible solutions include
 ; loop recur, conj and if. Remember to always terminate the recurring, or you will get infinite recursion.
 (defn product-is-available-recur [local-date product-coll]
-  (loop [cl product-coll result []]
-    (if-let [h (first cl)]
-      (recur (rest cl)
-        (conj result
-          (if (.isAvailable h local-date) h [])))
-      result)))
+  product-coll)
 
 ; Define which function you want to test here
 (def product-func product-is-available)
@@ -27,18 +21,21 @@
 (deftest empty-list-when-no-products
   (is (= (product-func (LocalDate.) []))))
 
+#_
 (deftest product-available-when-matching-date
   (let [today (new LocalDate)
         product (new Product "Cool product" today today)]
     (is (not-empty (product-func today [product])))
     (is (.contains (product-func today [product]) product))))
 
+#_
 (deftest empty-list-when-no-products-matching-date
   (let [today (new LocalDate)
         yesterday (.minusDays today 1)
         product (new Product "Cool product" yesterday yesterday)]
     (is (empty? (product-func today [product])))))
 
+#_
 (deftest filter-one-products-matching-date
   (let [today (new LocalDate)
         yesterday (.minusDays today 1)
@@ -46,6 +43,7 @@
         product-two (new Product "Todays product" today today)]
     (is (= 1 (count (product-func today [product-one product-two]))))))
 
+#_
 (deftest find-discontinued-products
   (let [yesterday (.minusDays (new LocalDate) 1)
         product (new Product "Cool product" yesterday yesterday)]
