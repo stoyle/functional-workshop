7b1b29a4f1ac4f3c51b493dcf5adf689b932ced5
Alf Kristian Stoyle <alf.kristian@gmail.com>
2012-03-18 17:01:17 +0100

    Euler one ready for being solved.

--- clojure-functional/src/test/clojure/no/knowit/clojure_functional/euler_one.clj
+++ clojure-functional/src/test/clojure/no/knowit/clojure_functional/euler_one.clj
@@ -14,28 +14,24 @@
   [coll] (apply + coll))
 
 (defn multiples-of-3-or-5 [num]
-  (let [mod-of #(zero? (mod num %1))]
-    (or (mod-of 3) (mod-of 5))))
+  )
 
 ;; Use standard filter, sum and range.
 (defn euler-one-sum-beneath [limit]
-  (sum (filter multiples-of-3-or-5 (range 1 limit))))
-
-(defn- sum-modof-coll [coll]
-  (reductions +
-    (filter multiples-of-3-or-5 coll)))
+  )
 
 ;; Try implementing it using a lazy collection. Hint reductions.
 (defn euler-one-sum-beneath-lazy [limit]
-  (last
-    (sum-modof-coll (range 1 limit))))
+  )
 
 
 ; Test code
+#_
 (deftest euler-one-sum
   (is (= (euler-one-sum-beneath 10) 23))
   (is (= (euler-one-sum-beneath 1000) 233168)))
 
+#_
 (deftest euler-one-lazy
   (is (= (euler-one-sum-beneath-lazy 10) 23))
   (is (= (euler-one-sum-beneath-lazy 1000) 233168)))
