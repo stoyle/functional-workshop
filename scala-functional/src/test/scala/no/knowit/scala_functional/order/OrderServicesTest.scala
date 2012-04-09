43babfe2ea19f348ac5e0fffe386e283ac502666
Fredrik Vraalsen <fredrik@vraalsen.no>
2012-04-09 22:12:03 +0200

    Added Scala-version of Order problem

---
+++ scala-functional/src/test/scala/no/knowit/scala_functional/order/OrderServicesTest.scala
@@ -0,0 +1,60 @@
+package no.knowit.scala_functional.order
+
+import org.hamcrest.CoreMatchers._
+import org.hamcrest.number.OrderingComparisons._
+import org.joda.time.DateTime
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import org.junit.Test
+import org.junit.Assert._
+import org.junit.matchers.JUnitMatchers._
+
+import no.knowit.java_functional.order.Order
+import no.knowit.java_functional.order.TrainJourney
+
+@RunWith(classOf[JUnit4])
+class OrderServicesTest {
+
+  def createOrderAlternatives(itineraries: Seq[TrainJourney]): Seq[Order] = itineraries map { createOrder(_) }
+
+  def createOrdersParallel(itineraries: Seq[TrainJourney]): Seq[Order] = itineraries.par.map(createOrder(_)).seq
+
+  private def createOrder(journey: TrainJourney): Order = {
+    try {
+      Thread.sleep(200)
+    } catch {
+      case e: InterruptedException => // Do nothing
+    }
+    new Order(journey, 123.0)
+  }
+
+  @Test
+  def convert_trainjourney_to_order() {
+    val itineraries = createItineraries("OSLO S", "BERGEN", new DateTime())
+
+    val orderAlternatives = createOrderAlternatives(itineraries)
+
+    assertThat(orderAlternatives.size, is(equalTo(5)))
+    assertTrue("Order missing price", orderAlternatives forall { _.getPrice > 0.0 })
+  }
+
+  @Test
+  def create_orders_within_timelimit() {
+    val itineraries = createItineraries("OSLO S", "BERGEN", new DateTime());
+
+    val startTime = System.currentTimeMillis();
+    val orderAlternatives = createOrdersParallel(itineraries);
+    val endTime = System.currentTimeMillis();
+    val duration = endTime - startTime;
+
+    assertThat(orderAlternatives.size, is(equalTo(5)));
+    assertTrue("Order missing price", orderAlternatives forall { _.getPrice > 0.0 })
+    assertTrue("Create order alternatives too fast", duration >= 200);
+    assertTrue("Create order alternatives took too long", duration < 600);
+  }
+
+  private def createItineraries(from: String, to: String, departureTime: DateTime): Seq[TrainJourney] =
+    for (i <- 1 to 5)
+      yield new TrainJourney(from, to, departureTime.plusHours(i), departureTime.plusHours(i + 2))
+
+}
