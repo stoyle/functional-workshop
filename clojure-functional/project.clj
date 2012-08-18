1ba16d637db7be5282c2195c8e2be87e887fa650
Alf Kristian Stoyle <alf.kristian@gmail.com>
2012-08-18 11:34:29 +0200

    Updated leiningen project to also use clojure 1.4.

--- clojure-functional/project.clj
+++ clojure-functional/project.clj
@@ -1,6 +1,6 @@
 (defproject fp "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [no.knowit/common "0.1-SNAPSHOT"]
                  [joda-time/joda-time "2.0"]]
   :source-path "src/main/clojure"
